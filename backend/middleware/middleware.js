2d94b67673fde240adcc9d442b89be528a686d26
badhon-dalbot <bdalbot116@gmail.com>
2025-01-05 02:31:16 +0600

    user route

---
+++ backend/middleware/middleware.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+const secretKey = "2c3f35b8a3988bed11689e3fc1aabe08064abd0d43";
+
+const checkLogin = (req, res, next) => {
+  const token = req.cookies.loggedInUser;
+
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, secretKey);
+      req.user = decoded;
+
+      if ((res, locals, html)) {
+        res.locals.loggedInUser = decoded;
+      }
+      next();
+    } catch (err) {
+      res.status(500).json({ err: "Authentication failure" });
+    }
+  } else {
+    return res.json({ err: "Your are not authenticated" });
+  }
+};
+
+module.exports = checkLogin;
--- backend/server.js
+++ backend/server.js
@@ -1,38 +1,28 @@
 const express = require("express");
 const mysql = require("mysql");
-const jwt = require("jsonwebtoken");
+const cookie = require("cookie-parser");
+
 const path = require("path"); // Add path module
 const cors = require("cors");
 const db = require("./database"); // Import the database connection
+const userRoute = require("./routes/userRoutes");
 const app = express();
 const port = 3000;
-// const secretKey = "2c3f35b8a3988bed11689e3fc1aabe08064abd0d43";
+const secretKey = "2c3f35b8a3988bed11689e3fc1aabe08064abd0d43";
 
 // Middleware to parse incoming requests
 app.use(express.json()); // To parse JSON bodies
 app.use(cors());
+app.use(cookie);
 app.use(express.urlencoded({ extended: true })); // To parse URL-encoded bodies
 
 // Middleware to serve static files
 app.use(express.static(path.join(__dirname, "../frontend"))); // This serves files from the frontend folder
 //middlware
-// const authenticateToken = (req, res, next) => {
-//   // const token = req.headers("Authorization")?.replace("Bearer ", "");
-//   const authHeader = req.headers["authorization"];
-//   const token = authHeader && authHeader.split(" ")[1];
-
-//   if (!token)
-//     return res.status(401).json({ error: "Access denied. No token provided" });
-
-//   jwt.verify(token, secretKey, (err, decoded) => {
-//     if (err) return res.status(403).json({ error: "invalid token" });
-
-//     res.id = decoded.id;
-//     next();
-//   });
-// };
 
 // Routes
+
+app.use("user/", userRoute);
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/index.html")); // Serve the index.html file
 });
@@ -40,6 +30,7 @@ app.get("/", (req, res) => {
 // Login API route
 app.post("/login", (req, res) => {
   const { username, password } = req.body;
+  // const token = req.cookies.loggedInUser;
 
   if (!username || !password) {
     return res
@@ -61,9 +52,19 @@ app.post("/login", (req, res) => {
       message: "login successful",
       username: user.username,
     });
-
-    // const token = jwt.sign({ id: userId }, secretKey, { expiresIn: "1h" });
+    //token
+    const token = jwt.sign(user, secretKey, { expiresIn: "1d" });
     // res.json({ token });
+
+    //set cookie
+    res.cookie("integrated-community-service", token, {
+      expires: new Date(Date.now() + 1 * 24 * 60 * 60 * 1000),
+      httpOnly: true,
+      signed: true,
+    });
+    return res.redirect("/");
+
+    // res.locals.loggedInUser = user;
 
     if (results.length > 0) {
       return res.status(200).json({ message: "Login successful" });
@@ -75,18 +76,19 @@ app.post("/login", (req, res) => {
 
 // Registration API route
 app.post("/register", (req, res) => {
-  const { full_name, username, email, password, phone_number, address, role } =
+  const { username, name, email, password, phone, city, district, division } =
     req.body;
 
   // Validation (Optional)
   if (
-    !full_name ||
     !username ||
+    !name ||
     !email ||
     !password ||
-    !phone_number ||
-    !address ||
-    !role
+    !phonenumber ||
+    !city ||
+    !district ||
+    !division
   ) {
     return res.status(400).json({ error: "All fields are required." });
   }
@@ -109,10 +111,10 @@ app.post("/register", (req, res) => {
 
     // Insert user into the database
     const query =
-      "INSERT INTO users (full_name, username,  email, password, phone_number, address, role) VALUES (?, ?, ?, ?, ?, ?, ?)";
+      "INSERT INTO users (username, name,  email, password, phonenumber, city, district, divison) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
     db.query(
       query,
-      [full_name, username, email, password, phone_number, address, role],
+      [username, name, email, password, phonenumber, city, district, division],
       (err, results) => {
         if (err) {
           console.error("Error during registration:", err);
@@ -128,28 +130,6 @@ app.post("/register", (req, res) => {
 });
 
 //profile
-app.get("/user/profile", (req, res) => {
-  // const userId = req.id;
-  // console.log(userId);
-  const { username } = req.query;
-  console.log(username);
-  if (!username) return res.status(400).json({ error: "user id required" });
-
-  const query = `SELECT user_id, full_name, username, email FROM users WHERE username = ?`;
-  db.query(query, [username], (err, results) => {
-    if (err || results.length === 0) {
-      res.status(500).json({ error: "Failed to fetch user data" });
-    }
-
-    const user = results[0];
-    res.json({
-      userId: user.user_id,
-      username: user.username,
-      fullName: user.full_name,
-      email: user.email,
-    });
-  });
-});
 
 // Update user Profile
 // app.put("/user/:id", (req, res) => {
